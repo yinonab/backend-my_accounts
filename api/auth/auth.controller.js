b3206c10aa1af66efa652327b28e7f2a71ae497b
yinonab <yinonabarjel@gmail.com>
2025-04-23 07:28:07 +0300

    201

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -146,7 +146,7 @@ export async function logout(req, res) {
 export async function ping(req, res) {
     try {
         const userId = req.user?.id || req.user?._id || "Unknown User";
-        console.log(`🔄 Keep-Alive Ping התקבל עבור userId: ${userId}`);
+        console.log(`🔄 Keep-Alive Ping התקבל`);
         res.status(200).send({ msg: "Session is active", userId });
     } catch (err) {
         console.log("⚠️ Ping Error:", err);
